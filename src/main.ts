9e5c44f9ddafb01fc61002a244c8f2a7232ed17a
juanxo13597 <juanxo97master@gmail.com>
2023-06-27 19:00:36 +0200

    change url openapi

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().setTitle('NTT API').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
